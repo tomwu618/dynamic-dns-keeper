e52a79b303bf6c77f0a01fb5a6fa4916961a394a
Tom吴 <luvnana618@gmail.com>
2022-12-01 15:40:44 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,10 @@ use crate::menu::build_menu;
 fn main() {
     let menu = build_menu();
     let toml_config = config::read_config(menu);
+
+    println!("waiting for network...");
+    thread::sleep(Duration::from_secs(60));
+    println!("start");
     cmd::run_array(toml_config.global.post_up.as_str());
 
     toml_config.record.iter().for_each(|r| {
