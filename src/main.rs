d04afc0fdb7028c0332a22a2d00846f669a7feee
Tom吴 <luvnana618@gmail.com>
2022-12-03 00:01:58 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::net::IpAddr;
 use std::str::FromStr;
 use std::thread;
 use std::time::Duration;
+use crate::init::init;
 
 mod config;
 mod menu;
@@ -16,27 +17,24 @@ pub mod record;
 mod cmd;
 mod function;
 mod cloudflare;
+mod init;
 
 use crate::menu::build_menu;
+use std::env;
 
 const DO_UPDATE_CMD_IP_LIST: Vec<String> = Vec::new();
 
 fn main() {
     let menu = build_menu();
     let toml_config = config::read_config(menu);
-
-    println!("waiting for network...");
-    thread::sleep(Duration::from_secs(toml_config.global.post_up_wait));
-    println!("start");
-    cmd::run_array(toml_config.global.post_up_cmd.as_str());
+    init(&toml_config);
 
     toml_config.record.iter().for_each(|r| {
         let record = r.clone();
         thread::spawn(move || {
             loop {
                 let fun = function::create(&record);
-
-                let ip_addr_result = cmd::run(record.get("ip_address_from").unwrap().as_str().unwrap());
+                let ip_addr_result = cmd::run(record.get("ip_address_from_cmd").unwrap().as_str().unwrap());
 
                 if !ip_addr_result.is_err() {
                     let ip_addr = ip_addr_result.unwrap();
@@ -44,11 +42,13 @@ fn main() {
                     if !ip.is_err() {
                         let updated = fun.update(ip.unwrap());
                         if updated {
-                            if !DO_UPDATE_CMD_IP_LIST.contains(&ip_addr) {
-                                cmd::run_array(record.get("on_update_cmd").unwrap().as_str().unwrap());
+                            let on_update_cmd = record.get("ip_address_on_update_cmd");
+                            if on_update_cmd.is_some() && !DO_UPDATE_CMD_IP_LIST.contains(&ip_addr) {
+                                let cmd_str= on_update_cmd.unwrap().as_str().unwrap().replace("${IP_ADDRESS}", &*ip_addr);
+                                cmd::run_array(cmd_str.to_string().as_str());
+
                                 DO_UPDATE_CMD_IP_LIST.push(ip_addr);
                             }
-
                             println!("updated");
                         }
                     }
